8bc1796b96a6ef370c4afb366b29aa9c0dfc094c
Szőcs Gábor <szocsgabor@turr.hu>
2026-02-14 15:00:44 +0100

    Add project files.

---
+++ FontViewerTest/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace FontViewerTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
